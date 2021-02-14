e23742a11e266b5e0f27bad515485d281db397c2
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2021-02-13 19:39:01 -0600

    use secrets instead of random

--- i.py
+++ i.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import random
+import secrets
 
 # TODO: get these  unicode hexagrams into the output
 """
@@ -15,7 +15,7 @@ def throw_hexagram():
     prob = [6, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 7, 7, 7, 7, 7]
     hexagram = []
     for i in range(0, 6):
-        x = random.choice(prob)
+        x = secrets.choice(prob)
         hexagram.append(x)
     return hexagram
 
@@ -33,74 +33,74 @@ def binary_hexagram(hexagram):
 
 def lookup_hexagram_number(hexagram):
     bingram = binary_hexagram(hexagram)
-    ahex = {}
-    ahex["111111"] = 1
-    ahex["000000"] = 2
-    ahex["100010"] = 3
-    ahex["010001"] = 4
-    ahex["111010"] = 5
-    ahex["010111"] = 6
-    ahex["010000"] = 7
-    ahex["000010"] = 8
-    ahex["111011"] = 9
-    ahex["110111"] = 10
-    ahex["111000"] = 11
-    ahex["000111"] = 12
-    ahex["101111"] = 13
-    ahex["111101"] = 14
-    ahex["001000"] = 15
-    ahex["000100"] = 16
-    ahex["100110"] = 17
-    ahex["011001"] = 18
-    ahex["110000"] = 19
-    ahex["000011"] = 20
-    ahex["100101"] = 21
-    ahex["101001"] = 22
-    ahex["000001"] = 23
-    ahex["100000"] = 24
-    ahex["100111"] = 25
-    ahex["111001"] = 26
-    ahex["100001"] = 27
-    ahex["011110"] = 28
-    ahex["010010"] = 29
-    ahex["101101"] = 30
-    ahex["001110"] = 31
-    ahex["011100"] = 32
-    ahex["001111"] = 33
-    ahex["111100"] = 34
-    ahex["000101"] = 35
-    ahex["101000"] = 36
-    ahex["101011"] = 37
-    ahex["110101"] = 38
-    ahex["001010"] = 39
-    ahex["010100"] = 40
-    ahex["110001"] = 41
-    ahex["100011"] = 42
-    ahex["111110"] = 43
-    ahex["011111"] = 44
-    ahex["000110"] = 45
-    ahex["011000"] = 46
-    ahex["010110"] = 47
-    ahex["011010"] = 48
-    ahex["101110"] = 49
-    ahex["011101"] = 50
-    ahex["100100"] = 51
-    ahex["001001"] = 52
-    ahex["001011"] = 53
-    ahex["110100"] = 54
-    ahex["101100"] = 55
-    ahex["001101"] = 56
-    ahex["011011"] = 57
-    ahex["110110"] = 58
-    ahex["010011"] = 59
-    ahex["110010"] = 60
-    ahex["110011"] = 61
-    ahex["001100"] = 62
-    ahex["101010"] = 63
-    ahex["010101"] = 64
+    bhex = {}
+    bhex["111111"] = 1
+    bhex["000000"] = 2
+    bhex["100010"] = 3
+    bhex["010001"] = 4
+    bhex["111010"] = 5
+    bhex["010111"] = 6
+    bhex["010000"] = 7
+    bhex["000010"] = 8
+    bhex["111011"] = 9
+    bhex["110111"] = 10
+    bhex["111000"] = 11
+    bhex["000111"] = 12
+    bhex["101111"] = 13
+    bhex["111101"] = 14
+    bhex["001000"] = 15
+    bhex["000100"] = 16
+    bhex["100110"] = 17
+    bhex["011001"] = 18
+    bhex["110000"] = 19
+    bhex["000011"] = 20
+    bhex["100101"] = 21
+    bhex["101001"] = 22
+    bhex["000001"] = 23
+    bhex["100000"] = 24
+    bhex["100111"] = 25
+    bhex["111001"] = 26
+    bhex["100001"] = 27
+    bhex["011110"] = 28
+    bhex["010010"] = 29
+    bhex["101101"] = 30
+    bhex["001110"] = 31
+    bhex["011100"] = 32
+    bhex["001111"] = 33
+    bhex["111100"] = 34
+    bhex["000101"] = 35
+    bhex["101000"] = 36
+    bhex["101011"] = 37
+    bhex["110101"] = 38
+    bhex["001010"] = 39
+    bhex["010100"] = 40
+    bhex["110001"] = 41
+    bhex["100011"] = 42
+    bhex["111110"] = 43
+    bhex["011111"] = 44
+    bhex["000110"] = 45
+    bhex["011000"] = 46
+    bhex["010110"] = 47
+    bhex["011010"] = 48
+    bhex["101110"] = 49
+    bhex["011101"] = 50
+    bhex["100100"] = 51
+    bhex["001001"] = 52
+    bhex["001011"] = 53
+    bhex["110100"] = 54
+    bhex["101100"] = 55
+    bhex["001101"] = 56
+    bhex["011011"] = 57
+    bhex["110110"] = 58
+    bhex["010011"] = 59
+    bhex["110010"] = 60
+    bhex["110011"] = 61
+    bhex["001100"] = 62
+    bhex["101010"] = 63
+    bhex["010101"] = 64
     with open("iching-title.txt", "r") as i:
         chis = i.readlines()
-    chi = chis[ahex[bingram] - 1]
+    chi = chis[bhex[bingram] - 1]
     return chi
 
 
